c6ef67a59066a157b463d1d5d4da8b4e90df9c47
travis.meador <t.meador@wearedoubleline.com>
2023-03-03 13:46:21 -0600

    REVERT once testing complete

--- Application/EdFi.Ods.AdminApp.Web/Artifacts/MsSql/Structure/Admin/202204250900-RemoveAzureSqlConfigurationsTable.sql
+++ Application/EdFi.Ods.AdminApp.Web/Artifacts/MsSql/Structure/Admin/202204250900-RemoveAzureSqlConfigurationsTable.sql
@@ -3,4 +3,10 @@
 -- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 -- See the LICENSE and NOTICES files in the project root for more information.
 
-DROP TABLE IF EXISTS adminapp.AzureSqlConfigurations;
+-- DROP TABLE IF EXISTS adminapp.AzureSqlConfigurations;
+
+IF EXISTS (SELECT 1 FROM sys.tables WHERE name = 'AzureSqlConfigurations')
+BEGIN
+    EXEC( 'DROP TABLE adminapp.AzureSqlConfigurations' )
+END
+
